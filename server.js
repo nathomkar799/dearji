8d3dafe67eef39a67830c8c5698aba5651406d4e
Omkar Nath <nathomkar799@gmail.com>
2025-09-06 16:59:19 +0530

    Added port

--- server.js
+++ server.js
@@ -33,4 +33,5 @@ app.use('/auth', routeToAuth);
 app.use('/customer', routeToAddCustomerDetails, addMeasurements )
 
 //listening
-app.listen(3000, ()=> console.log("🚀 Server running on port 3000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log("🚀 Server running on port 3000"));
