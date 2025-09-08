3e6e567f0da0414994a312abadb7b769609b1321
Omkar Nath <nathomkar799@gmail.com>
2025-09-08 19:59:07 +0530

    commented one of the console.log

--- controllers/customer.js
+++ controllers/customer.js
@@ -17,7 +17,7 @@ async function handleSearchResult(req, res) {
 
     if (customer) {
       // found customer
-      console.log(customer.remarks);
+      // console.log(customer.remarks);
       
       return res.render("crud", { customer, searched: true, updation : false });
     } else {
@@ -63,8 +63,8 @@ async function handleAddNewCustomer(req, res) {
 
      // Generate QR for the customer profile URL
      
-    //  const url = `http://localhost:3000/customer/${customer._id}`;
-     const url = `https://dearji.onrender.com/customer/${customer._id}`;
+     const url = `http://localhost:3000/customer/${customer._id}`;
+    //  const url = `https://dearji.onrender.com/customer/${customer._id}`;
 
      const qrCodeData = await QRCode.toDataURL(url);
 
